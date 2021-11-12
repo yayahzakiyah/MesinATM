8b1644a04f49985c2ef110d1e801622b69961b44
Yayah Zakiyah <86923817+yayahzakiyah@users.noreply.github.com>
2021-11-12 21:04:54 +0700

    Add files via upload

---
+++ atm_card.py
@@ -0,0 +1,11 @@
+#Membuat class ATMCard yang memiliki fungsi __init__ untuk menerima argumen nilai untuk pin awal dn saldo awal
+class ATMCard:
+    def __init__(self, defaultPin, defaultBalance):
+        self.defaultPin = defaultPin
+        self.defaultBalance = defaultBalance
+
+def cekPinAwal(self):
+    return self.defaultPin
+
+def cekSaldoAwal(self):
+    return self.defaultBalance
